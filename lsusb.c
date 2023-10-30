luckman19980301/usbutils

commit 8a80f70a8afb2f6831b150a69cdf8f566deefe66

names.c: if a string can not be found in the usb.ids file, return [unknown]
For interfaces or other USB strings, if they are not in the current
hardware database, return "[unknown]" to give people a chance to
understand why their device isn't reporting anything for those fields.

This changes the output of a line that previous looked like:
	|__ Port 004: Dev 006, If 0, Class=, Driver=, 12M
to be:
	|__ Port 004: Dev 006, If 0, Class=[unknown], Driver=, 12M

Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

names.c
@@ -192,7 +192,7 @@ int get_vendor_string(char *buf, size_t size, uint16_t vid)
                 return 0;
         *buf = 0;
         if (!(cp = names_vendor(vid)))
-                return 0;
+		return snprintf(buf, size, "[unknown]");
         return snprintf(buf, size, "%s", cp);
 }
 
@@ -204,7 +204,7 @@ int get_product_string(char *buf, size_t size, uint16_t vid, uint16_t pid)
                 return 0;
         *buf = 0;
         if (!(cp = names_product(vid, pid)))
-                return 0;
+		return snprintf(buf, size, "[unknown]");
         return snprintf(buf, size, "%s", cp);
 }
 
@@ -216,7 +216,7 @@ int get_class_string(char *buf, size_t size, uint8_t cls)
 		return 0;
 	*buf = 0;
 	if (!(cp = names_class(cls)))
-		return 0;
+		return snprintf(buf, size, "[unknown]");
 	return snprintf(buf, size, "%s", cp);
 }
 
@@ -228,7 +228,7 @@ int get_subclass_string(char *buf, size_t size, uint8_t cls, uint8_t subcls)
 		return 0;
 	*buf = 0;
 	if (!(cp = names_subclass(cls, subcls)))
-		return 0;
+		return snprintf(buf, size, "[unknown]");
 	return snprintf(buf, size, "%s", cp);
 }
 

commit d5889329c9777c0a63ed066e1e185999787e336c

lsusb: fix up [unknown] vendor and product strings.
In commit 8a80f70a8afb ("names.c: if a string can not be found in the
usb.ids file, return [unknown]"), the logic for determining the vendor
and product for the USB device was changed to ONLY rely on the usb.ids
file, instead of falling back to any vendor/product strings that are on
the device itself.

This had the bad side-affect of causing many devices that previously
showed vendor/product information just fine, to now show up as
[unknown].

Fix this up to fall back to looking at the string in the device itself,
and ONLY if the string is not present, will '[unknown]' be printed out.

This changes the output on one of my machines from:
	Bus 003 Device 002: ID 32ac:0002 [unknown] [unknown]
to:
	Bus 003 Device 002: ID 32ac:0002 Framework HDMI Expansion Card

This should resolve github issue #177, many thanks to the people
reporting it quickly there.

Fixes: 8a80f70a8afb ("names.c: if a string can not be found in the usb.ids file, return [unknown]")
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

lsusb.c
@@ -3624,11 +3624,11 @@ static void get_vendor_product_with_fallback(char *vendor, int vendor_len,
 
 	if (get_sysfs_name(sysfs_name, sizeof(sysfs_name), dev) >= 0) {
 		if (!have_vendor)
-			read_sysfs_prop(vendor, vendor_len, sysfs_name,
-					"manufacturer");
+			if (!read_sysfs_prop(vendor, vendor_len, sysfs_name, "manufacturer"))
+				strncpy(vendor, "[unknown]", vendor_len);
 		if (!have_product)
-			read_sysfs_prop(product, product_len, sysfs_name,
-					"product");
+			if (!read_sysfs_prop(product, product_len, sysfs_name, "product"))
+				strncpy(product, "[unknown]", product_len);
 	}
 }
 

names.c
@@ -192,7 +192,7 @@ int get_vendor_string(char *buf, size_t size, uint16_t vid)
                 return 0;
         *buf = 0;
         if (!(cp = names_vendor(vid)))
-		return snprintf(buf, size, "[unknown]");
+		return 0;
         return snprintf(buf, size, "%s", cp);
 }
 
@@ -204,7 +204,7 @@ int get_product_string(char *buf, size_t size, uint16_t vid, uint16_t pid)
                 return 0;
         *buf = 0;
         if (!(cp = names_product(vid, pid)))
-		return snprintf(buf, size, "[unknown]");
+		return 0;
         return snprintf(buf, size, "%s", cp);
 }
 