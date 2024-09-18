luckman19980301/usbutils

commit 86dcab8964901d15b87b156546559c8ae28bd9fe

usbutils: convert build system to use meson
Autotools is great for what it is designed for, but that epoch is long
ago, and shipping random files/scripts that are not actually checked
into the build system is ripe for problems.

Convert the build over to use meson instead.  Bonus is that the build
logic is simpler (if not still messy in places), and that the build
itself is way faster.

Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

autogen.sh
@@ -1,45 +1,6 @@
 #!/bin/sh -e
 # SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2009,2010 Greg Kroah-Hartman <gregkh@suse.de>
+# Copyright (c) 2009,2010,2024 Greg Kroah-Hartman <gregkh@suse.de>
 
-if test ! -f usbhid-dump/bootstrap; then
-git submodule update --init --recursive
-fi
-
-cd usbhid-dump
-./bootstrap
-cd ..
-
-#gtkdocize
-autoreconf --install --symlink
-
-MYCFLAGS="-g -Wall \
--Wmissing-declarations -Wmissing-prototypes \
--Wnested-externs -Wpointer-arith \
--Wpointer-arith -Wsign-compare -Wchar-subscripts \
--Wstrict-prototypes -Wshadow \
--Wformat=2 -Wtype-limits"
-
-case "$CFLAGS" in
-	*-O[0-9]*)
-		;;
-	*)
-		MYCFLAGS="$MYCFLAGS -O2"
-		;;
-esac
-
-libdir() {
-	echo $(cd $1/$(gcc -print-multi-os-directory); pwd)
-}
-
-args="--prefix=/usr \
---sysconfdir=/etc \
---sbindir=/sbin \
---libdir=$(libdir /usr/lib) \
---with-rootlibdir=$(libdir /lib) \
---libexecdir=/lib/udev \
---with-selinux \
---enable-gtk-doc"
-
-export CFLAGS="$CFLAGS $MYCFLAGS"
-./configure $args $@
+meson setup build
+meson compile -C build

.travis.yml
@@ -17,10 +17,7 @@ matrix:
 addons:
     apt:
         packages:
-            - autoconf
-            - automake
-            - libtool
-            - m4
+            - meson
             - libudev-dev
             - libusb-dev
             - libusb-1.0-0-dev
@@ -28,5 +25,5 @@ addons:
             - ubuntu-toolchain-r-test
 
 script:
-    - ./autogen.sh && make -j4 && make maintainer-clean
+    - ./autogen.sh
     - if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then ./travis-autogen.sh && make -j4; fi

Makefile.am
@@ -1,95 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (c) 2004 Thomas Sailer <sailer@ife.ee.ethz.ch>
-# Copyright (c) 2005-2006 David Brownell <dbrownell@users.sourceforge.net>
-# Copyright (c) 2009 Kay Sievers <kay.sievers@vrfy.org>
-# Copyright (c) 2009 Greg Kroah-Hartman <gregkh@suse.de>
-
-SUBDIRS = \
-	usbhid-dump
-
-AM_LDFLAGS = \
-	-Wl,--as-needed
-
-data_DATA =
-
-bin_PROGRAMS = \
-	lsusb
-
-sbin_SCRIPTS =
-
-bin_SCRIPTS = \
-	usb-devices \
-	lsusb.py
-
-noinst_PROGRAMS = \
-	usbreset
-
-lsusb_SOURCES = \
-	lsusb.c lsusb.h \
-	lsusb-t.c \
-	list.h \
-	desc-defs.c desc-defs.h \
-	desc-dump.c desc-dump.h \
-	names.c names.h \
-	sysfs.c sysfs.h \
-	usb-spec.h \
-	usbmisc.c usbmisc.h
-
-lsusb_CPPFLAGS = \
-	$(AM_CPPFLAGS) $(LIBUSB_CFLAGS) $(UDEV_CFLAGS)
-
-lsusb_LDADD = \
-	$(LIBUSB_LIBS) \
-	$(UDEV_LIBS)
-
-usbreset_SOURCES = \
-	usbreset.c
-
-man_MANS = \
-	lsusb.8	\
-	lsusb.py.1	\
-	usbhid-dump.8 \
-	usb-devices.1 \
-	usbreset.1
-
-EXTRA_DIST = \
-	lsusb.8.in \
-	lsusb.py.1.in \
-	usbhid-dump.8.in \
-	usb-devices.1.in \
-	usb-devices \
-	lsusb.py.in \
-	usbreset.1.in \
-	usbreset.c \
-	LICENSES/GPL-2.0-only.txt \
-	LICENSES/GPL-3.0-only.txt
-
-lsusb.py: $(srcdir)/lsusb.py.in
-	sed 's|VERSION|$(VERSION)|g;s|@usbids@|$(datadir)/usb.ids|g' $< >$@
-	chmod 755 $@
-
-lsusb.8: $(srcdir)/lsusb.8.in
-	sed 's|VERSION|$(VERSION)|g' $< >$@
-
-lsusb.py.1: $(srcdir)/lsusb.py.1.in
-	sed 's|VERSION|$(VERSION)|g' $< >$@
-
-usbhid-dump.8: $(srcdir)/usbhid-dump.8.in
-	sed 's|VERSION|$(VERSION)|g' $< >$@
-
-usb-devices.1: $(srcdir)/usb-devices.1.in
-	sed 's|VERSION|$(VERSION)|g' $< >$@
-
-usbreset.1: $(srcdir)/usbreset.1.in
-	sed 's|VERSION|$(VERSION)|g' $< >$@
-
-DISTCLEANFILES = \
-	lsusb.py \
-	lsusb.py.1 \
-	lsusb.8 \
-	usb-devices.1 \
-	usbreset.1 \
-	usbhid-dump.8
-
-distclean-local:
-	rm -rf autom4te.cache

configure.ac
@@ -1,43 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2009 Kay Sievers <kay.sievers@vrfy.org>
-
-AC_INIT([usbutils],
-	[017],
-	[linux-usb@vger.kernel.org])
-AC_PREREQ(2.60)
-AM_INIT_AUTOMAKE([check-news foreign 1.9 dist-bzip2])
-m4_ifdef([AM_SILENT_RULES], [AM_SILENT_RULES([yes])])
-AC_PREFIX_DEFAULT([/usr])
-
-AC_USE_SYSTEM_EXTENSIONS
-AC_SYS_LARGEFILE
-
-PKG_INSTALLDIR
-
-PKG_CHECK_MODULES(LIBUSB, libusb-1.0 >= 1.0.14)
-
-PKG_CHECK_MODULES(UDEV, libudev >= 196)
-
-AC_CONFIG_HEADERS([config.h])
-AC_CONFIG_FILES([
-	Makefile
-])
-AC_CONFIG_SUBDIRS([usbhid-dump])
-
-AC_OUTPUT
-AC_MSG_RESULT([
-	usbutils $VERSION
-	=============
-
-	prefix:                 ${prefix}
-	datarootdir:            ${datarootdir}
-	datadir:                ${datadir}
-	pkgconfigdir:		${pkgconfigdir}
-	mandir:                 ${mandir}
-
-	usb.ids:                ${datadir}/usb.ids
-
-	compiler:               ${CC}
-	cflags:                 ${CFLAGS}
-	ldflags:                ${LDFLAGS}
-])

lsusb.8.in
@@ -1,6 +1,6 @@
 .\"SPDX-License-Identifier: GPL-2.0-only
 .\"Copyright (c) 1999 Thomas Sailer <sailer@ife.ee.ethz.ch>
-.TH lsusb 8 "11 February 2019" "usbutils-VERSION" "Linux USB Utilities"
+.TH lsusb 8 "11 February 2019" "usbutils-@VERSION@" "Linux USB Utilities"
 .IX lsusb
 .SH NAME
 lsusb \- list USB devices

lsusb.c
@@ -3715,7 +3715,7 @@ int main(int argc, char *argv[])
 			long_options, NULL)) != EOF) {
 		switch (c) {
 		case 'V':
-			printf("lsusb (" PACKAGE ") " VERSION "\n");
+			printf("lsusb (" PACKAGE_NAME ") " VERSION "\n");
 			return EXIT_SUCCESS;
 		case 'v':
 			verblevel++;

lsusb.py.1.in
@@ -1,7 +1,7 @@
 .\"SPDX-License-Identifier: GPL-2.0-only
 .\"Copyright (c) 1999 Thomas Sailer <sailer@ife.ee.ethz.ch>
 .\"Copyright (c) 2020 Sam Morris <sam@robots.org.uk>
-.TH lsusb.py 1 "03 January 2024" "usbutils-VERSION" "Linux USB Utilities"
+.TH lsusb.py 1 "03 January 2024" "usbutils-@VERSION@" "Linux USB Utilities"
 .IX lsusb.py
 .SH NAME
 lsusb.py \- list USB devices

lsusb.py.in
@@ -29,7 +29,7 @@ showwakeup = False
 
 prefix = "/sys/bus/usb/devices/"
 usbids = [
-	"@usbids@",
+	"@DATADIR@/usb.ids",
 	"/usr/share/hwdata/usb.ids",
 	"/usr/share/usb.ids",
 ]

commit 20ae721361ffde5fd650f3d60309bd210d2b9a39

autogen.sh: use valid email for 2024 copyright
Pretty sure the gregkh@suse.de email is no longer active.

Fixes: 86dcab8 ("usbutils: convert build system to use meson")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 # SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2009,2010,2024 Greg Kroah-Hartman <gregkh@suse.de>
+# Copyright (c) 2009,2010 Greg Kroah-Hartman <gregkh@suse.de>
+# Copyright (c) 2024 Greg Kroah-Hartman <gregkh@linuxfoundation.org>
 
 meson setup build
 meson compile -C build